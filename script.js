95a6de605c58c087b0c1c986f2301256028f0472
venkatsundaraj <venkateshsundarasan@gmail.com>
2022-10-15 09:18:51 +0530

    second commt

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const imgContainer = document.querySelector(".image-container");
-const text = document.querySelector(".text-container");
+const text = document.querySelector("p");
 
 document.addEventListener("scroll", function () {
   const y = window.scrollY;
-  const frame = Math.min(Math.floor(y / 15) + 2, 63);
-  console.log(y, frame);
+  const frame = Math.min(Math.floor(y / 15) + 1, 63);
+  // console.log(y, frame);
 
   imgContainer.style.backgroundImage = `url(./images/ezgif-frame-${frame
     .toString()
     .padStart(3, "0")}.png)`;
 
-  text.style.opacity = `${frame + 20}`;
+  text.style.zIndex = "10";
+  text.style.color = `rgba(255, 255, 255, ${(frame / 62) * 1})`;
+  console.log(frame);
 });
